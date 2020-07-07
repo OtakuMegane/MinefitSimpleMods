ff2d20b3a2fcae4d2670b0b7226b5e4140da7318
OtakuMegane <otakumegane@insaneotaku.com>
2020-07-07 16:20:38 -0400

    .

--- src/main/java/com/minefit/xerxestireiron/minefitsimplemods/RandomMOTD.java
+++ src/main/java/com/minefit/xerxestireiron/minefitsimplemods/RandomMOTD.java
@@ -9,26 +9,23 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
-public class RandomMOTD implements Listener
-{
-	private final MinefitSimpleMods plugin;
-	private final Random random = new Random();
-	private final List<String> MOTDList;
-	private int listLength = 0;
+public class RandomMOTD implements Listener {
+    private final MinefitSimpleMods plugin;
+    private final Random random = new Random();
+    private final List<String> MOTDList;
+    private int listLength = 0;
 
-	public RandomMOTD(MinefitSimpleMods instance)
-	{
-		this.plugin = instance;
-		this.MOTDList = this.plugin.main_config.getStringList("RandomMOTD.messages");
-		this.listLength = this.MOTDList.size();
-	}
+    public RandomMOTD(MinefitSimpleMods instance) {
+        this.plugin = instance;
+        this.MOTDList = this.plugin.main_config.getStringList("RandomMOTD.messages");
+        this.listLength = this.MOTDList.size();
+    }
 
-	@EventHandler(priority = EventPriority.NORMAL)
-	private void onServerPing(ServerListPingEvent event)
-	{
-		if(this.listLength != 0)
-		{
-			event.setMotd(ChatColor.translateAlternateColorCodes('&', this.MOTDList.get(this.random.nextInt(this.listLength))));
-		}
-	}
+    @EventHandler(priority = EventPriority.NORMAL)
+    private void onServerPing(ServerListPingEvent event) {
+        if (this.listLength != 0) {
+            event.setMotd(ChatColor.translateAlternateColorCodes('&',
+                    this.MOTDList.get(this.random.nextInt(this.listLength))));
+        }
+    }
 }
